e0dc912e8eb9d970046e000dc94e033ae03fe62b
201244076 LDH <7825dfg@naver.com>
2016-11-25 11:32:55 +0900

    수업자료 1번 완료 / 사진파일 3개삭제, 3개추가 / alert창 미구현

--- app/src/main/java/com/example/ldh/android20161118/MainActivity.java
+++ app/src/main/java/com/example/ldh/android20161118/MainActivity.java
@@ -4,58 +4,60 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button button, btn1, btn2;
     TextView t1, t2;
-    CheckBox cb;
+    Switch sw;
     RadioButton r1, r2, r3;
     RadioGroup rg;
     ImageView iview;
+    LinearLayout LL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("애완동물 사진보기");
+        setTitle("안드로이드 사진보기");
         button = (Button)findViewById(R.id.button);
         t1 = (TextView)findViewById(R.id.textview1);
         t2 = (TextView)findViewById(R.id.textView2);
-        cb = (CheckBox)findViewById(R.id.checkBox);
+        sw = (Switch) findViewById(R.id.switch1);
         rg = (RadioGroup)findViewById(R.id.rg);
         r1 = (RadioButton)findViewById(R.id.radioButton1);
         r2 = (RadioButton)findViewById(R.id.radioButton2);
         r3 = (RadioButton)findViewById(R.id.radioButton3);
         iview =(ImageView)findViewById(R.id.imageView);
+        btn1 = (Button)findViewById(R.id.button2);
+        btn2 = (Button)findViewById(R.id.button3);
+        LL = (LinearLayout)findViewById(R.id.LL);
 
+        LL.setVisibility(View.INVISIBLE);
         iview.setVisibility(View.INVISIBLE);
-        t2.setVisibility(View.INVISIBLE);
-        rg.setVisibility(View.INVISIBLE);
-        button.setVisibility(View.INVISIBLE);
 
-        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+
+
+
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
 
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked ==true){
-                    t2.setVisibility(View.VISIBLE);
-                    rg.setVisibility(View.VISIBLE);
-                    button.setVisibility(View.VISIBLE);
+                    LL.setVisibility(View.VISIBLE);
 
                 }
                 else {
-                    t2.setVisibility(View.INVISIBLE);
-                    rg.setVisibility(View.INVISIBLE);
-                    button.setVisibility(View.INVISIBLE);
-                    iview.setVisibility(View.INVISIBLE);
+                   LL.setVisibility(View.INVISIBLE);
                 }
             }
         });
@@ -66,34 +68,39 @@ public class MainActivity extends AppCompatActivity {
                 if(group.getId()==R.id.rg){
                     switch (checkedId){
                         case R.id.radioButton1 :
-                            Toast.makeText(getApplicationContext(),"강아지를 선택하였습니다.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(),"젤리빈을 선택하였습니다.", Toast.LENGTH_SHORT).show();
+                            iview.setImageResource(R.drawable.zel);
+                            iview.setVisibility(View.VISIBLE);
                             break;
                         case R.id.radioButton2 :
-                            Toast.makeText(getApplicationContext(), "고양이를 선택하였습니다.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "킷캣을 선택하였습니다.", Toast.LENGTH_SHORT).show();
+                            iview.setImageResource(R.drawable.kit);
+                            iview.setVisibility(View.VISIBLE);
                             break;
                         case R.id.radioButton3 :
-                            Toast.makeText(getApplicationContext(), "토끼를 선택하였습니다.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "롤리팝을 선택하였습니다.", Toast.LENGTH_SHORT).show();
+                            iview.setImageResource(R.drawable.lol);
+                            iview.setVisibility(View.VISIBLE);
                             break;
                     }
                 }
             }
         });
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(r1.isChecked()){
-                    iview.setImageResource(R.drawable.dog);
-                    iview.setVisibility(View.VISIBLE);
-                }
-                else if(r2.isChecked()){
-                    iview.setImageResource(R.drawable.cat);
-                    iview.setVisibility(View.VISIBLE);
-                }
-                else if(r3.isChecked()){
-                    iview.setImageResource(R.drawable.rabbit);
-                    iview.setVisibility(View.VISIBLE);
-                }
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sw.setChecked(false);
+                LL.setVisibility(View.INVISIBLE);
+                r1.setChecked(true);
+
             }
         });
     }
